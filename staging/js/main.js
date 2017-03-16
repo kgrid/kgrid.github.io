0bc2ca418912671dee06fc3de539e30577a848f7
George Meng <gqmeng@umich.edu>
2017-03-16 13:07:12 -0400

    Modifications on nav bar and associated responsive behaviors.

--- staging/js/main.js
+++ staging/js/main.js
@@ -1,12 +1,62 @@
 (function ($) {
 
+	var scrollbarwidth=getScrollbarWidth();
+	var viewportWidth = $(window).width()+scrollbarwidth;
+	var viewportHeight = $(window).height()+scrollbarwidth;
+		console.log(scrollbarwidth+"Width: "+viewportWidth+"   Height: "+viewportHeight);
+	var reftop=50;
+	var adjratio=1.5;
+	var sizew=45;
+	if(viewportWidth>767) {
+		reftop=120;
+		adjratio=1.7;
+		sizew=35;
+	}
+	var klogotop= $('#kgridlogo').offset().top - $(window).scrollTop()
+	console.log("Start at:"+klogotop);
+
+  //Manage button
+	$('.navbar-collapse').on('hidden.bs.collapse', function () {
+	  $("button.navbar-toggle>span").addClass("fa-bars");
+		$("button.navbar-toggle>span").removeClass("fa-times");
+	});
+	$('.navbar-collapse').on('show.bs.collapse', function () {
+		$("button.navbar-toggle>span").removeClass("fa-bars");
+		$("button.navbar-toggle>span").addClass("fa-times");
+	});
+
+	$(window).resize(function(){
+		var viewportWidth = $(window).width()+scrollbarwidth;
+		var viewportHeight = $(window).height()+scrollbarwidth;
+		console.log("Width: "+viewportWidth+"   Height: "+viewportHeight);
+	});
+
+
 	$(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
 			$('.scrollup').fadeIn();
 			} else {
 				$('.scrollup').fadeOut();
 			}
+
+			//Manage logo
+			var klogotop= $('#kgridlogo').offset().top - $(window).scrollTop()
+			var w=(1-(reftop-klogotop)/reftop/adjratio)*100;
+			if(w>100) w=100;
+			var h= $("div#kgridlogo img").height();
+			console.log("top@"+klogotop+" w="+w);
+
+			if(w>sizew){
+						$("div#kgridlogo img").css("width",w+"%");
+						$("a.navbar-brand").removeClass("show");
+						$("#navlogo").parent().parent().css("background-color","transparent");
+			}else{
+						$("a.navbar-brand").addClass("show");
+						$("#navlogo").parent().parent().css("background-color","#fff");
+			}
+
 		});
+
 		$('.scrollup').click(function(){
 			$("html, body").animate({ scrollTop: 0 }, 1000);
 				return false;
@@ -15,64 +65,11 @@
 	// local scroll
 	jQuery('.navbar').localScroll({hash:true, offset: {top: 0},duration: 800, easing:'easeInOutExpo'});
 
-
-	// portfolio
-    if($('.isotopeWrapper').length){
-
-        var $container = $('.isotopeWrapper');
-        var $resize = $('.isotopeWrapper').attr('id');
-        // initialize isotope
-
-        $container.isotope({
-            itemSelector: '.isotopeItem',
-            resizable: false, // disable normal resizing
-            masonry: {
-                columnWidth: $container.width() / $resize
-            }
-
-
-
-        });
-
-        $('#filter a').click(function(){
-
-
-
-            $('#filter a').removeClass('current');
-            $(this).addClass('current');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 1000,
-                    easing: 'easeOutQuart',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-
-        $(window).smartresize(function(){
-            $container.isotope({
-                // update columnWidth to a percentage of container width
-                masonry: {
-                    columnWidth: $container.width() / $resize
-                }
-            });
-        });
-
-
-}
-
-
 	// fancybox
 	jQuery(".fancybox").fancybox();
 
-
 	if (Modernizr.mq("screen and (max-width:1024px)")) {
 			jQuery("body").toggleClass("body");
-
 	} else {
 		var s = skrollr.init({
 			mobileDeceleration: 1,
@@ -89,67 +86,43 @@
 			});
 	}
 
-
-
 	//scroll menu
 	jQuery('.appear').appear();
 	jQuery(".appear").on("appear", function(data) {
 			var id = $(this).attr("id");
 			jQuery('.nav li').removeClass('active');
 			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");
-		});
+	});
 
-		jQuery(".story-appear").appear();
-		jQuery(".story-appear").on("appear", function(data) {
+	//Scroll to show/hide stories
+	jQuery(".story-appear").appear();
+	jQuery(".story-appear").on("appear", function(data) {
 			$(this).addClass("active");
+	});
+	jQuery(".story-appear").on("disappear", function(data) {
+ 			$(this).removeClass("active");
+	});
 
-		});
-		jQuery(".story-appear").on("disappear", function(data) {
-			// $(this).removeClass("active");
+	function getScrollbarWidth() {
+	    var outer = document.createElement("div");
+	    outer.style.visibility = "hidden";
+	    outer.style.width = "100px";
+	    document.body.appendChild(outer);
 
-		});
+	    var widthNoScroll = outer.offsetWidth;
+	    // force scrollbars
+	    outer.style.overflow = "scroll";
 
+	    // add innerdiv
+	    var inner = document.createElement("div");
+	    inner.style.width = "100%";
+	    outer.appendChild(inner);
 
-		//parallax
-        var isMobile = false;
+	    var widthWithScroll = inner.offsetWidth;
 
-        if(Modernizr.mq('only all and (max-width: 1024px)') ) {
-            isMobile = true;
-        }
+	    // remove divs
+	    outer.parentNode.removeChild(outer);
 
-
-        if (isMobile == false && ($('#parallax1').length  ||isMobile == false &&  $('#parallax2').length ||isMobile == false &&  $('#testimonials').length))
-        {
-
-
-            $(window).stellar({
-                responsive:true,
-                scrollProperty: 'scroll',
-                parallaxElements: false,
-                horizontalScrolling: false,
-                horizontalOffset: 0,
-                verticalOffset: 0
-            });
-
-        }
-
-    //Google Map
-    // var get_latitude = $('#google-map').data('latitude');
-    // var get_longitude = $('#google-map').data('longitude');
-		//
-    // function initialize_google_map() {
-    //     var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
-    //     var mapOptions = {
-    //         zoom: 14,
-    //         scrollwheel: false,
-    //         center: myLatlng
-    //     };
-    //     var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-    //     var marker = new google.maps.Marker({
-    //         position: myLatlng,
-    //         map: map
-    //     });
-    // }
-    // google.maps.event.addDomListener(window, 'load', initialize_google_map);
-
+	    return widthNoScroll - widthWithScroll;
+	}
 })(jQuery);
