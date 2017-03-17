d3cc21f9fa6530df29fa84869c13537124fac2f4
George Meng <gqmeng@umich.edu>
2017-03-17 12:04:06 -0400

    Modifications on the navbar for responsive behaviors for small and large screen, need more work on some intermediate screen sizes.

--- staging/js/main.js
+++ staging/js/main.js
@@ -1,66 +1,67 @@
 (function ($) {
-
 	var scrollbarwidth=getScrollbarWidth();
 	var viewportWidth = $(window).width()+scrollbarwidth;
 	var viewportHeight = $(window).height()+scrollbarwidth;
-		console.log(scrollbarwidth+"Width: "+viewportWidth+"   Height: "+viewportHeight);
-	var reftop=50;
-	var adjratio=1.5;
-	var sizew=45;
+	var blogo_w=$('#blogo').width();
+	var blogo_h=$('#blogo').width();
+
+	var adjratio=0.45;
 	if(viewportWidth>767) {
-		reftop=120;
-		adjratio=1.7;
-		sizew=35;
+		adjratio=0.75;
 	}
-	var klogotop= $('#kgridlogo').offset().top - $(window).scrollTop()
-	console.log("Start at:"+klogotop);
-
-  //Manage button
-	$('.navbar-collapse').on('hidden.bs.collapse', function () {
-	  $("button.navbar-toggle>span").addClass("fa-bars");
-		$("button.navbar-toggle>span").removeClass("fa-times");
-	});
-	$('.navbar-collapse').on('show.bs.collapse', function () {
-		$("button.navbar-toggle>span").removeClass("fa-bars");
-		$("button.navbar-toggle>span").addClass("fa-times");
-	});
-
-	$(window).resize(function(){
-		var viewportWidth = $(window).width()+scrollbarwidth;
-		var viewportHeight = $(window).height()+scrollbarwidth;
-		console.log("Width: "+viewportWidth+"   Height: "+viewportHeight);
-	});
-
+	var navOffset = $("#kgridlogo").offset().top;
+  $("#kgridlogo").wrap('<div id="logowrapper"></div>');
 
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollup').fadeIn();
-			} else {
-				$('.scrollup').fadeOut();
-			}
-
-			//Manage logo
-			var klogotop= $('#kgridlogo').offset().top - $(window).scrollTop()
-			var w=(1-(reftop-klogotop)/reftop/adjratio)*100;
+		// if ($(this).scrollTop() > 100) {
+		// 	$('.scrollup').fadeIn();
+		// 	} else {
+		// 		$('.scrollup').fadeOut();
+		// 	}
+				//Manage logo
+			var klogotop= $('#kgridlogo').offset().top - $(window).scrollTop();
+			var scrolltop = $(window).scrollTop();
+			var w=(1-(navOffset-klogotop)*adjratio/navOffset)*100;
 			if(w>100) w=100;
-			var h= $("div#kgridlogo img").height();
-			console.log("top@"+klogotop+" w="+w);
-
-			if(w>sizew){
-						$("div#kgridlogo img").css("width",w+"%");
-						$("a.navbar-brand").removeClass("show");
-						$("#navlogo").parent().parent().css("background-color","transparent");
+			console.log("L-Top@"+klogotop+" W-Top@"+scrolltop+" N-Offset@"+navOffset+" w="+w);
+			$("#logowrapper").height($("#kgridlogo").outerHeight(false));
+			$("div#kgridlogo").css("width",w+"%");
+			console.log("Div width="+$("div#kgridlogo").height());
+			if(scrolltop<=navOffset){
+						$('#kgridlogo').removeClass("fixed");
+						$(".navbar").css("background-color","transparent");
 			}else{
-						$("a.navbar-brand").addClass("show");
-						$("#navlogo").parent().parent().css("background-color","#fff");
+						$('#kgridlogo').addClass("fixed");
+						$(".navbar").css("background-color","#fff");
 			}
 
 		});
 
-		$('.scrollup').click(function(){
-			$("html, body").animate({ scrollTop: 0 }, 1000);
-				return false;
-		});
+
+
+		  //Manage button
+			$('.navbar-collapse').on('hidden.bs.collapse', function () {
+			  $("button.navbar-toggle>span").addClass("fa-bars");
+				$("button.navbar-toggle>span").removeClass("fa-times");
+			});
+			$('.navbar-collapse').on('show.bs.collapse', function () {
+				$("button.navbar-toggle>span").removeClass("fa-bars");
+				$("button.navbar-toggle>span").addClass("fa-times");
+			});
+
+			$(window).resize(function(){
+				var viewportWidth = $(window).width()+scrollbarwidth;
+				var viewportHeight = $(window).height()+scrollbarwidth;
+				console.log("Width: "+viewportWidth+"   Height: "+viewportHeight);
+				$("html, body").animate({ scrollTop: 0 }, 1000);
+				navOffset = $("#kgridlogo").offset().top;
+			});
+
+
+		// $('.scrollup').click(function(){
+		// 	$("html, body").animate({ scrollTop: 0 }, 1000);
+		// 		return false;
+		// });
 
 	// local scroll
 	jQuery('.navbar').localScroll({hash:true, offset: {top: 0},duration: 800, easing:'easeInOutExpo'});
