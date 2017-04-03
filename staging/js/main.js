0385fe9f3a45c08885f7ac6f4519c269bb181b98
George Meng <gqmeng@umich.edu>
2017-04-02 21:09:56 -0400

    Added back the tech-tool section with new icons; Moved the K-grid to nav bar

--- staging/js/main.js
+++ staging/js/main.js
@@ -1,5 +1,8 @@
 (function ($) {
 
+		$("html").niceScroll({cursorwidth: '10', zindex: 9999,cursorcolor: "#e7e7e7", cursorborder:"1px solid #e7e7e7", cursorborderradius: "10px",autohidemode: false, zindex: 999 });
+
+		$.force_appear();
 	  //Manage button
 			$('.navbar-collapse').on('hidden.bs.collapse', function () {
 			  $("button.navbar-toggle>span").addClass("fa-bars");
