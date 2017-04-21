bc2af10994c3ca7db1d4bd008bc21293ba3d1043
George Meng <gqmeng@umich.edu>
2017-04-21 12:31:05 -0400

    Implemented the effect for the hamburger toggle; Modified the styles for drop down menu items; Modified the sidebar nav checkpoint;

--- staging/js/main.js
+++ staging/js/main.js
@@ -3,25 +3,18 @@
 		$("html").niceScroll({cursorwidth: '10', zindex: 9999,cursorcolor: "#e7e7e7", cursorborder:"1px solid #e7e7e7", cursorborderradius: "10px",autohidemode: false, zindex: 999 });
 
 		$.force_appear();
-	  //Manage button
-		$('.navbar-collapse').on('hidden.bs.collapse', function () {
-		  $("button.navbar-toggle>span").addClass("fa-bars");
-			$("button.navbar-toggle>span").removeClass("fa-times");
-		});
 
-		$('.navbar-collapse').on('show.bs.collapse', function () {
-			$("button.navbar-toggle>span").removeClass("fa-bars");
-			$("button.navbar-toggle>span").addClass("fa-times");
-		});
-
+		$(".navbar-toggle").on("click", function () {
+	 				    $(this).toggleClass("active");
+	 			  });
 		onload = onresize = function() {
 			var viewport_width = $(window).width();
 
 			if(viewport_width>991){
-				console.log("LG"+viewport_width);
+
 				jQuery('#amid-parent').height(jQuery('#amid-side').outerHeight());
 			}else{
-				console.log("SM"+viewport_width);
+
 				jQuery('#amid-parent').height(jQuery('#amid-parent .align-mid').outerHeight()+50);
 			}
 			var maxHeight = 0;
--- staging/js/tool.js
+++ staging/js/tool.js
@@ -3,16 +3,6 @@
 		$("html").niceScroll({cursorwidth: '10', zindex: 9999,cursorcolor: "#e7e7e7", cursorborder:"1px solid #e7e7e7", cursorborderradius: "10px",autohidemode: false, zindex: 999 });
 
 		$.force_appear();
-	  //Manage button
-		$('.navbar-collapse').on('hidden.bs.collapse', function () {
-			  $("button.navbar-toggle>span").addClass("fa-bars");
-				$("button.navbar-toggle>span").removeClass("fa-times");
-		});
-
-		$('.navbar-collapse').on('show.bs.collapse', function () {
-				$("button.navbar-toggle>span").removeClass("fa-bars");
-				$("button.navbar-toggle>span").addClass("fa-times");
-		});
 
 		jQuery('.sidenav li:first-child').addClass('active');
 
@@ -34,6 +24,9 @@
 					});
 			}
 
+			$(".navbar-toggle").on("click", function () {
+		 				    $(this).toggleClass("active");
+		 			  });
 	//scroll menu
 		jQuery('.appear').appear();
 		jQuery(".appear").on("appear", function(data) {
@@ -54,14 +47,8 @@
 				}if(ratio>1){
 						ratio=1;
 				}
-
 				var stub_h= 40*ratio;
-				console.log("Window:"+st+" "+ id+ " T:"+pos.top+" H:"+h+" R:"+ratio);
-
-
-				 $(".sidenav>li.active>span.stub").css("height",stub_h+"px");
-
-
+			 $(".sidenav>li.active>span.stub").css("height",stub_h+"px");
 		},50))
 
 })(jQuery);
