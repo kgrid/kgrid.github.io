a031e86190371f691d853db52f1f66e263a28b31
George Meng <gqmeng@umich.edu>
2017-04-21 12:34:50 -0400

    Sync all files  to /site

--- site/js/main.js
+++ site/js/main.js
@@ -3,19 +3,20 @@
 		$("html").niceScroll({cursorwidth: '10', zindex: 9999,cursorcolor: "#e7e7e7", cursorborder:"1px solid #e7e7e7", cursorborderradius: "10px",autohidemode: false, zindex: 999 });
 
 		$.force_appear();
-	  //Manage button
-		$('.navbar-collapse').on('hidden.bs.collapse', function () {
-		  $("button.navbar-toggle>span").addClass("fa-bars");
-			$("button.navbar-toggle>span").removeClass("fa-times");
-		});
 
-		$('.navbar-collapse').on('show.bs.collapse', function () {
-			$("button.navbar-toggle>span").removeClass("fa-bars");
-			$("button.navbar-toggle>span").addClass("fa-times");
-		});
-
+		$(".navbar-toggle").on("click", function () {
+	 				    $(this).toggleClass("active");
+	 			  });
 		onload = onresize = function() {
-			jQuery('#amid-parent').height(jQuery('#amid-side').outerHeight());
+			var viewport_width = $(window).width();
+
+			if(viewport_width>991){
+
+				jQuery('#amid-parent').height(jQuery('#amid-side').outerHeight());
+			}else{
+
+				jQuery('#amid-parent').height(jQuery('#amid-parent .align-mid').outerHeight()+50);
+			}
 			var maxHeight = 0;
 			$(".use-detail").each(function() {
   			if ($(this).outerHeight() > maxHeight) {
@@ -27,6 +28,24 @@
 	 	jQuery('.navbar').localScroll({hash:true, offset: {top: 0},duration: 800, easing:'easeInOutExpo'});
 
 		jQuery('.nav li:first-child').addClass('active');
+
+		if (Modernizr.mq("screen and (max-width:1024px)")) {
+					jQuery("body").toggleClass("body");
+			} else {
+				var s = skrollr.init({
+					mobileDeceleration: 1,
+					edgeStrategy: 'set',
+					forceHeight: true,
+					smoothScrolling: true,
+					smoothScrollingDuration: 300,
+						easing: {
+							WTF: Math.random,
+							inverted: function(p) {
+								return 1-p;
+							}
+						}
+					});
+			}
 
 	//scroll menu
 		jQuery('.appear').appear();
--- site/js/tool.js
+++ site/js/tool.js
@@ -3,19 +3,30 @@
 		$("html").niceScroll({cursorwidth: '10', zindex: 9999,cursorcolor: "#e7e7e7", cursorborder:"1px solid #e7e7e7", cursorborderradius: "10px",autohidemode: false, zindex: 999 });
 
 		$.force_appear();
-	  //Manage button
-		$('.navbar-collapse').on('hidden.bs.collapse', function () {
-			  $("button.navbar-toggle>span").addClass("fa-bars");
-				$("button.navbar-toggle>span").removeClass("fa-times");
-		});
-
-		$('.navbar-collapse').on('show.bs.collapse', function () {
-				$("button.navbar-toggle>span").removeClass("fa-bars");
-				$("button.navbar-toggle>span").addClass("fa-times");
-		});
 
 		jQuery('.sidenav li:first-child').addClass('active');
 
+		if (Modernizr.mq("screen and (max-width:1024px)")) {
+					jQuery("body").toggleClass("body");
+			} else {
+				var s = skrollr.init({
+					mobileDeceleration: 1,
+					edgeStrategy: 'set',
+					forceHeight: true,
+					smoothScrolling: true,
+					smoothScrollingDuration: 300,
+						easing: {
+							WTF: Math.random,
+							inverted: function(p) {
+								return 1-p;
+							}
+						}
+					});
+			}
+
+			$(".navbar-toggle").on("click", function () {
+		 				    $(this).toggleClass("active");
+		 			  });
 	//scroll menu
 		jQuery('.appear').appear();
 		jQuery(".appear").on("appear", function(data) {
@@ -23,5 +34,21 @@
 			jQuery('.sidenav li').removeClass('active');
 			jQuery(".sidenav a[href='#" + id + "']").parent().addClass("active");
 		});
+
+		$(window).scroll(_.throttle(function(){
+				var st = $(window).scrollTop();
+				var wh= $(window).height();
+				var id = $(".sidenav>li.active:first>a").attr("href");
+				var pos = $(id).offset();
+				var h = $(id).outerHeight();
+				var ratio = (pos.top+h -st-wh+200)/h;
+				if(ratio<0) {
+					ratio=0;
+				}if(ratio>1){
+						ratio=1;
+				}
+				var stub_h= 40*ratio;
+			 $(".sidenav>li.active>span.stub").css("height",stub_h+"px");
+		},50))
 
 })(jQuery);
