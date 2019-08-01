47230d4ed481c0e806c808359b1434cbbf1feea8
George Meng <gqmeng.meng@gmail.com>
2019-08-01 13:44:45 -0400

    [#158041529] [#167432909] Clean up the unneeded files

--- js/main.js
+++ js/main.js
@@ -47,28 +47,7 @@
 			}
 
 		};
-	// local scroll
-	 	jQuery('.navbar').localScroll({hash:true, offset: {top: 0},duration: 800, easing:'easeInOutExpo'});
 
-		// jQuery('.navbar-nav>li:first-child').addClass('active');
-
-		if (Modernizr.mq("screen and (max-width:1024px)")) {
-					jQuery("body").toggleClass("body");
-			} else {
-				var s = skrollr.init({
-					mobileDeceleration: 1,
-					edgeStrategy: 'set',
-					forceHeight: true,
-					smoothScrolling: true,
-					smoothScrollingDuration: 300,
-						easing: {
-							WTF: Math.random,
-							inverted: function(p) {
-								return 1-p;
-							}
-						}
-					});
-			}
 			$('.dropdown').on('show.bs.dropdown', function(e){
 			  $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
 			});
